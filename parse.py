b6c6ef79e1a18836b7140c582fbc837f3023c682
David Ellis <isv.damocles@gmail.com>
2023-06-07 16:13:12 -0500

    More verbose prompt generation (#33)
    * WIP more verbose prompt generation
    
    * Write a nearly-complete markdown serializer on top of the AST

--- parse.py
+++ parse.py
@@ -1,6 +1,120 @@
 from mistletoe import Document, ast_renderer
 
 
+def to_markdown(node):
+    # Technically I should iterate on the `children` lists every time because they could have more
+    # than one, but since this is hardwired for each node type, I'm just going to use the actual
+    # implementations to skip that when possible to reduce recursion depth and simplify the code
+    if node['type'] == 'AutoLink':
+        return f'''[{node['children'][0]['content']}]'''
+    if node['type'] == 'BlockCode':
+        return '\n'.join([f'''    {line}''' for line in node['children'][0].split('\n')])
+    if node['type'] == 'CodeFence':
+        return f'''```{node['language']}
+{node['children'][0]['content']}
+```'''
+    if node['type'] == 'Document':
+        return ''.join([to_markdown(child) for child in node['children']])
+    if node['type'] == 'Emphasis':
+        return f'''*{node['children'][0]['content']}*'''
+    if node['type'] == 'EscapeSequence':
+        return f'''\\{node['children'][0]['content']}'''
+    if node['type'] == 'Heading':
+        return ('#' * node['level']) + ' ' + ''.join([to_markdown(child) for child in node['children']])
+    if node['type'] == 'Image':
+        if len(node['title']['children'][0]['content']) > 0:
+            return f'''![{''.join([to_markdown(child) for child in node['children']])}]({node['src']['children'][0]['content']} "{node['title']['children'][0]['content']}")'''
+        else:
+            return f'''![{''.join([to_markdown(child) for child in node['children']])}]({node['src']['children'][0]['content']})'''
+    if node['type'] == 'InlineCode':
+        return f'''`{node['children'][0]['content']}`'''
+    if node['type'] == 'LineBreak':
+        return '\n'
+    if node['type'] == 'Link':
+        if len(node['title']['children'][0]['content']) > 0:
+            return f'''[{''.join([to_markdown(child) for child in node['children']])}]({node['src']['children'][0]['content']} "{node['title']['children'][0]['content']}")'''
+        else:
+            return f'''[{''.join([to_markdown(child) for child in node['children']])}]({node['src']['children'][0]['content']})'''
+    if node['type'] == 'List':
+        if node['start'] is not None:
+            return '\n'.join([f'''{i}. {text}''' for (i, text) in enumerate([to_markdown(child) for child in node['children']])])
+        else:
+            return '\n'.join([f'''* {to_markdown(child)}''' for child in node['children']])
+    if node['type'] == 'ListItem':
+        return ''.join([to_markdown(child) for child in node['children']])
+    if node['type'] == 'Paragraph':
+        return ''.join([to_markdown(child) for child in node['children']])
+    if node['type'] == 'Quote':
+        return '\n'.join([f'''> {to_markdown(child)}''' for child in node['children']])
+    if node['type'] == 'RawText':
+        return node['content']
+    if node['type'] == 'SetextHeading':
+        raise NotImplementedError()
+    if node['type'] == 'Strikethrough':
+        return f'''~~{node['children'][0]['content']}~~'''
+    if node['type'] == 'Strong':
+        return f'''**{node['children'][0]['content']}**'''
+    if node['type'] == 'Table':
+        raise NotImplementedError()
+    if node['type'] == 'TableCell':
+        raise NotImplementedError()
+    if node['type'] == 'TableRow':
+        raise NotImplementedError()
+    if node['type'] == 'ThematicBreak':
+        return '\n---\n'
+    raise Exception(f'''Unknown AST node {node['type']} encountered!''')
+
+
+def format_func_for_llm(func):
+    ast = ast_renderer.get_ast(Document(func))
+    if ast['children'][0]['type'] != 'Heading':
+        raise Exception('Invalid Marsha function')
+    name = ''
+    args = []
+    ret = ''
+    desc_parts = []
+    reqs = ''
+    list_started = False
+    for (i, child) in enumerate(ast['children']):
+        if i == 0:
+            # Special handling for the initial header (for now)
+            if child['type'] != 'Heading':
+                raise Exception('Invalid Marsha function')
+            header = child['children'][0]['content']
+            name = header.split('(')[0].split('func')[1].strip()
+            args = [arg.strip() for arg in header.split('(')[1].split(')')[0].split(',')]
+            ret = header.split('):')[1].strip()
+            continue
+        if child['type'] == 'List':
+            list_started = True
+            reqs = to_markdown(child)
+            continue
+        if list_started:
+            raise Exception('Function description must come *before* usage examples')
+        desc_parts.append(to_markdown(child))
+    desc = '\n\n'.join(desc_parts)
+
+    arg_fmt = '\n'.join([f'{i + 1}. {arg}' for (i, arg) in enumerate(args)])
+
+    return f'''# Requirements for function `{name}`
+
+## Inputs
+
+{arg_fmt}
+
+## Output
+
+{ret}
+
+## Description
+
+{desc}
+
+## Examples of expected behavior
+
+{reqs}'''
+
+
 def extract_function_name(func):
     ast = ast_renderer.get_ast(Document(func))
     if ast['children'][0]['type'] != 'Heading':
