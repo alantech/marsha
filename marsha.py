ec9f9629bb3388499da04ecfb23544f2d7e6ce91
David Ellis <isv.damocles@gmail.com>
2023-06-07 19:53:32 -0500

    Parallelize the first stage (#34)

--- marsha.py
+++ marsha.py
@@ -19,6 +19,7 @@ parser = argparse.ArgumentParser(
     description='Marsha AI Compiler',
 )
 parser.add_argument('source')
+parser.add_argument('-d', '--debug', action='store_true', help='Turn on debug logging')
 args = parser.parse_args()
 
 
@@ -40,12 +41,40 @@ async def main():
     func = f.read()
     f.close()
     files = write_files_from_markdown(await gpt_func_to_python(func))
+    if args.debug:
+        for file in files:
+            print(file)
+            f = open(file, 'r')
+            print(f.read())
+            f.close()
+            print()
     print('Parsing generated code...')
     await lint_and_fix_files(files)
+    if args.debug:
+        for file in files:
+            print(file)
+            f = open(file, 'r')
+            print(f.read())
+            f.close()
+            print()
     print('Verifying and correcting generated code...')
     await test_and_fix_files(func, files)
+    if args.debug:
+        for file in files:
+            print(file)
+            f = open(file, 'r')
+            print(f.read())
+            f.close()
+            print()
     print('Formatting code...')
     autoformat_files(files)
+    if args.debug:
+        for file in files:
+            print(file)
+            f = open(file, 'r')
+            print(f.read())
+            f.close()
+            print()
     t2 = time.time()
     print(f'Done! Total time elapsed: {prettify_time_delta(t2 - t1)}')
 
