54352fd91309e6402994a012e8eaa7a572061d46
David Ellis <isv.damocles@gmail.com>
2023-06-12 12:23:54 -0500

    Add docstrings and type hints to generated python code (#39)

--- examples/fibonacci/fibonacci.py
+++ examples/fibonacci/fibonacci.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
+def fibonacci(n: int) -> int:
+  """This function calculates the nth fibonacci number, where n is provided to it and starts with 1.
+
+  fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)"""
   if n <= 0:
     raise Exception('The fibonacci sequence only exists in positive whole number space')
   elif n == 1 or n == 2:
