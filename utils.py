829b17b253465994df686c58ac56d2dd3fc6ed4e
Alejandro Guillen <aguillenv93@gmail.com>
2023-07-03 22:03:39 +0200

    Generate multiple marsha fns and types using only one prompt to one file (#69)
    * first stage changes
    
    * add second and third stage
    
    * add marsh examples
    
    * Integrate types in one file prompt
    
    * Refactor and clean marsha file
    
    * clean parse
    
    * clean and update llm
    
    * add utils
    
    * fix code not awaited, debug and wrong param
    
    * remove examples
    
    * fix stats
    
    * remove examples
    
    * remove py
    
    * update gitingore
    
    * move from directory
    
    * update gitignore
    
    * restore test folder
    
    * move fns to utils
    
    ---------
    
    Co-authored-by: Luis Fernando <lfdepombo@gmail.com>

---
+++ utils.py
@@ -0,0 +1,44 @@
+import autopep8
+import os
+import shutil
+
+
+def read_file(filename: str, mode: str = 'r'):
+    f = open(filename, mode)
+    content = f.read()
+    f.close()
+    return content
+
+
+def write_file(filename: str, content: str, mode: str = 'w'):
+    f = open(filename, mode)
+    f.write(content)
+    f.close()
+
+
+def autoformat_files(files: list[str]):
+    for file in files:
+        before = read_file(file)
+        after = autopep8.fix_code(before)
+        write_file(file, after)
+
+
+def copy_file(src: str, dest: str):
+    shutil.copyfile(src, dest)
+
+
+def delete_dir_and_content(filename: str):
+    dir = os.path.dirname(filename)
+    if os.path.isdir(dir):
+        shutil.rmtree(dir)
+
+
+def get_filename_from_path(path: str):
+    return os.path.splitext(os.path.basename(path))[0]
+
+
+def get_file_fullpath(filename) -> str:
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file == filename:
+                return os.path.join(root, file)
