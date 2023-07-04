e2862919c2644ab3675c6ea8d31d857cf5b8b18b
Alejandro Guillen <aguillenv93@gmail.com>
2023-07-04 19:44:38 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='marsha',
+    version='0.0.1',
+    description='A example Python package',
+    url='https://github.com/alantech/marsha',
+    author='Alan Technologies Maintainers',
+    author_email='hello@alantechnologies.com',
+    license='BSD 2-clause',
+    packages=['marsha'],
+    install_requires=[
+        'autopep8',
+        'flake8',
+        'mccabe',
+        'mistletoe',
+        'openai',
+        'pycodestyle',
+        'pydocstyle',
+        'pyflakes',
+        'pyinstaller',
+        'pylama'
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
