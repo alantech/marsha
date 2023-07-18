fc3854dbd3e615a9ee9a842e245b5e448d2f4bd8
David Ellis <isv.damocles@gmail.com>
2023-07-18 18:25:40 -0500

    Add a basic REST web server

--- marsha/utils.py
+++ marsha/utils.py
@@ -50,26 +50,69 @@ if __name__ == '__main__':
     parser.add_argument('--stdin', action='store_true', required=False)
     parser.add_argument('--infile', action='store', required=False, default=None)
     parser.add_argument('--outfile', action='store', required=False, default=None)
+    parser.add_argument('--serve', action='store', required=False, type=int)
     parser.add_argument('params', nargs='*')
     args = parser.parse_args()
     func = lookup[args.func]
-    out = None
-    if args.stdin:
-        import sys
-        param = sys.stdin.read()
-        out = func(param)
-    elif args.infile is not None:
-        file = open(args.infile, 'r')
-        param = file.read()
-        file.close()
-        out = func(param)
+    if args.serve is not None:
+        from http.server import BaseHTTPRequestHandler, HTTPServer
+        import json
+        class MarshaServer(BaseHTTPRequestHandler):
+            def do_POST(self):
+                func_name = self.path.split('/')[1]
+                if not func_name in func_names:
+                    self.send_response(404)
+                    self.send_header('Content-Type', 'application/json')
+                    self.end_headers()
+                    self.wfile.write(bytes('{"error": "' + self.path + ' does not exist"}', 'utf-8'))
+                    return
+                content_len = int(self.headers.get('Content-Length'))
+                post_body = self.rfile.read(content_len)
+                post_payload = None
+                try:
+                    post_payload = json.loads(post_body)
+                except:
+                    self.send_response(400)
+                    self.send_header('Content-Type', 'application/json')
+                    self.end_headers()
+                    self.wfile.write(bytes('{"error": "Invalid JSON provided"}', 'utf-8'))
+                    return
+                out = None
+                if type(post_payload) is list:
+                    out = func(*post_payload)
+                else:
+                    out = func(post_payload)
+                self.send_response(200)
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+                self.wfile.write(bytes(json.dumps(out), 'utf-8'))
+        server = HTTPServer(('', args.serve), MarshaServer)
+        print(f'Listening on port {args.serve}')
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            pass
+
+        server.server_close()
+        print("Server stopped.")
     else:
-        out = func(*args.params)
-    if args.outfile is not None:
-        file = open(args.outfile, 'w')
-        file.write(out)
-        file.close()
-    else:
-        print(out)
+        out = None
+        if args.stdin:
+            import sys
+            param = sys.stdin.read()
+            out = func(param)
+        elif args.infile is not None:
+            file = open(args.infile, 'r')
+            param = file.read()
+            file.close()
+            out = func(param)
+        else:
+            out = func(*args.params)
+        if args.outfile is not None:
+            file = open(args.outfile, 'w')
+            file.write(out)
+            file.close()
+        else:
+            print(out)
 """)
     f.close()
